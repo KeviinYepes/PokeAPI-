6afaa5bb7d37eaf6f774ea6c00248041c82da649
KeviinYepes <keyv2412@gmail.com>
2025-09-05 15:57:55 -0500

    first update PokeApi using flask

---
+++ app/database.py
@@ -0,0 +1,18 @@
+import requests
+
+def ApiRequest(): 
+    URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+    response = requests.get(URL)
+    if response.status_code == 200:
+        print("Connected")
+        data = response.json()   
+        results = data['results']   
+        pokemons = []    
+        for pokemon in results:
+            obj = {"Pokemon":pokemon['name'] , "Img": pokemon['url']}
+            pokemons.append(obj)
+        return pokemons
+    else:
+        print("Problems:", response.text)
+
+
